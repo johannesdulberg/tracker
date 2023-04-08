a64509c47b71ff5242226162b3a8cb51a2a285fb
Johannes Dülberg <johannesdulberg@Johanness-MacBook-Pro-4.local>
2023-04-08 10:04:08 +0200

    tailwind installed

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('tracker/', include('tracker.urls')),
     path('authuser/', include('authuser.urls')),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
