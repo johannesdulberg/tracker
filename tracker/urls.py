696e09ed447a55da682784c946dd830e08109847
Johannes Dülberg <johannesdulberg@Johanness-MacBook-Pro-4.local>
2023-04-18 02:35:56 +0200

    fitness

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 app_name = "tracker"
 urlpatterns = [
-
+    path('', views.index, name='index'),
+    path('record/', views.record, name='record'),
+    path('createWorkout/', views.createWorkout, name='createWorkout'),
+    path('metrics/', views.chart_data, name="metrics"),
 ]
